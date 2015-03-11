6d534087f973b2dab75329081810e6013ac598d3
DrAlexMV <alexandermventura@utexas.edu>
2015-03-11 13:48:31 -0500

    init

---
+++ configuration/config.py
@@ -0,0 +1,6 @@
+config = {
+    "DATABASE_NAME": "banana-fish",
+    "DATABASE_PORT": "",
+    "DATABASE_HOST": "",
+    "API_VER": "v1",
+}
---
+++ models/Game.py
@@ -0,0 +1,20 @@
+__author__ = 'alexanderventura'
+
+from configuration.app import db
+
+
+class Game(db.Model):
+    # id = db.Column(db.bbb)
+    # genre
+    # release_date
+    # description
+    # platform
+    # company
+    # reviews
+
+
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        return ""
