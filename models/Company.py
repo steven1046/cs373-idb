78418def28d8a60745426f1725fdb5525be5ba51
Nicolas Vasquez <nicoavasquez@yahoo.com>
2015-03-22 19:22:39 -0500

    Working on Game and Company model. Models match the schema for the tables in data_collection/create_tables.sql

--- models/Company.py
+++ models/Company.py
@@ -1,1 +1,47 @@
 __author__ = 'alexanderventura'
+
+from configuration.database import db
+from sqlalchemy.dialects.postgresql import ARRAY
+
+class Company(db.Model) :
+    __tablename__ = "companies"
+
+    company_id = db.Column(db.Integer, primarykey=True)
+    name = db.Column(db.String(80))
+    deck = db.Column(db.Text)
+    description = db.Column(db.Text)
+    image = db.Column(db.String(80))
+    address = db.Column(db.String(80))
+    city = db.Column(db.String(50))
+    state = db.Column(db.String(50))
+    country = db.Column(db.String(50))
+    phone = db.Column(db.String(20))
+    date_founded = db.Column(db.DateTime)
+    website = db.Column(db.String(80))
+
+    def __init__(self, company_id, name, deck, description, image, address, city, state, country, phone, date_founded, website) :
+    	self.company_id = company_id
+    	self.name = name
+    	self.deck = deck
+    	self.description = description
+    	self.image = image
+    	self.address = address
+    	self.city = city
+    	self.state = state
+    	self.country = country
+    	self.phone = phone
+    	self.date_founded = date_founded
+    	self.website = website
+
+    def __repr__(self) :
+    	return ''
+
+def create_company(company) :
+	new_company = Company(company["company_id"], company["name"], company["deck"], company["description"],
+						company["image"], company["address"], company["city"], company["state"], company["country"], 
+						company["phone"], company["date_founded"], company["website"])
+	db.session.add(new_company)
+	db.session.commit()
+
+
+
--- models/Game.py
+++ models/Game.py
@@ -7,24 +7,28 @@ from sqlalchemy.dialects.postgresql import ARRAY
 class Game(db.Model):
     __tablename__ = 'games'
 
-    id = db.Column(db.Integer, primary_key=True)
+    game_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
-    # genres = db.Column(ARRAY)
-    release_date = db.Column(db.DateTime)
-    description = db.Column(db.String(1000))
-    # platforms
-    # company
-    # reviews
+    image = db.Column(db.String(80))
+    original_release_date = db.Column(db.DateTime)
+    deck = db.Column(db.Text)
+    description = db.Column(db.Text)
+    company_id = db.Column(db.Integer)
 
-    def __init__(self, name, description):
+    def __init__(self, game_id, name, image, original_release_date, deck, description, company_id) :
+        self.game_id = game_id
         self.name = name
+        self.image = image
+        self.original_release_date = original_release_date
+        self.deck = deck
         self.description = description
+        self.company_id = company_id
 
     def __repr__(self):
         return ''
 
 
 def create_game(game):
-    new_game = Game(game["name"], game["description"])
+    new_game = Game(game["game_id"], game["name"], game["image"], game["original_release_date"], game["deck"], game["description"], game["company_id"])
     db.session.add(new_game)
     db.session.commit()
