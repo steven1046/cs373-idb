9c96de13ffe3095fbc83bfdeee6563cc1cc4ad7f
DrAlexMV <alexandermventura@utexas.edu>
2015-03-25 14:17:14 -0500

    cors

--- configuration/app.py
+++ configuration/app.py
@@ -1,9 +1,11 @@
+from flask_cors import CORS
 from flask import Flask
 from configuration.config import config, test_config
 
 app = Flask(__name__)
 
 app.config.update(config)
+cors = CORS(app)
 
 test_app = Flask(__name__)
 test_app.config.from_object(test_config)
