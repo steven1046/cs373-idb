588093a6917b4b81deca86ad65636bbbd21ef97d
LHBDev <freeraprb@gmail.com>
2015-04-09 14:04:14 -0500

    Test of job scrape script

--- data_collection/indeed_scripty.py
+++ data_collection/indeed_scripty.py
@@ -1,1 +1,17 @@
 import fileinput, requests, json, psycopg2, indeed
+
+client = IndeedClient('1942078712727036')
+
+
+
+def main():
+
+	conn_string = "host='localhost' dbname='banana-fish' user='dataUser' password='password!'"
+	conn = psycopg2.connect(conn_string)
+	cur = conn.cursor()
+	cur.execute("""SELECT * from Companies""")
+	rows = cur.fetchall()
+
+	print "\nShow me the databases:\n"
+	for row in rows:
+		print ("   ", row[0])
